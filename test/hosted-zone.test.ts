a0b7d81bcf5baa659cf64f3c9b813360952edffd
Sunjay <sdsunjay@users.noreply.github.com>
2022-11-01 17:56:30 -0700

    Initial commit

---
+++ test/hosted-zone.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HostedZone from '../lib/hosted-zone-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hosted-zone-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HostedZone.HostedZoneStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
